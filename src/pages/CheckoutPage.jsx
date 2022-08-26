3e39bb502f9be8d6cbc7753d32d524a34fc96e68
5iQWebAppReactJsRepoUser-at-703429964476 <anup@technostacks.in>
2022-08-26 17:12:05 +0530

    checked

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -272,7 +272,7 @@ const CheckoutPage = () => {
           <div>
             <section className="checkout-section">
               <div className="left">
-                <h1>SHIPPING ADDRESS check 777</h1>
+                <h1>SHIPPING ADDRESS check 888</h1>
                 <div>
                   <form
                     className="checkout-form"
